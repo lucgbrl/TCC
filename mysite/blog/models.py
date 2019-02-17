520bb5786a4be3ba1285b49d91100125cc0d0890
Handerson Cordeiro <handerson.tecavauninta@gmail.com>
2019-02-17 10:57:41 -0300

    Update models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,15 +4,26 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    #inteiros
+    idade = models.IntegerField()
+    #usuario que realiza o cadastro
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #cadastro de titulo
     title = models.CharField(max_length=200)
+
+    #textos longos 
     text = models.TextField()
+
+    #data e hora automatica
     created_date = models.DateTimeField(default=timezone.now)
+   
+    #cadastro de data
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+    #######
 
     def __str__(self):
         return self.title
