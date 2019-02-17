239dbb3c35909dac1fb601d5842b2780280b668b
lucgbrl <lucassantos@alu.ufc.br>
2019-02-17 11:34:04 -0300

    Dentro do arquivo .admin.py foi adicionada a chamada para as aplicações que serão controladas*

--- mysite/napi/admin.py
+++ mysite/napi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Prontuario_de_Atendimento_Integral_PAI
+from .exchange import Acompanhamento_de_Pressao_Arterial_Glicemia
 
-# Register your models here.
+# Registrando os models provenientes do arquivo .models aqui
+admin.site.register(Prontuario_de_Atendimento_Integral_PAI)
+
+# registrando arquivos provenientes do arquivos .exchange aqui
+admin.site.register(Acompanhamento_de_Pressao_Arterial_Glicemia)
