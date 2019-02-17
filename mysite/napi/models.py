d8d67cd3ff675bd888117ac239aeb9ad986f7b07
lucgbrl <lucassantos@alu.ufc.br>
2019-02-17 10:47:26 -0300

    Ficha de acompanhamento

--- mysite/napi/models.py
+++ mysite/napi/models.py
@@ -10,6 +10,21 @@ class PAI(models.Model):
 
     def publish(self):
         self.published_date = timezone.now()
+
+class Acomp_pressao_arterial_glicemia(models.Model):
+    responsavel = models.ForeignKey("responsavel pela solicitação", settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pai = models.ForeignKey("Pai", 'PAI', on_delete=models.CASCADE)
+    data_solicitacao = models.DateTimeField(blank=True, null=True)
+    dia = models.DateTimeField("Dia",default=timezone.now)
+    turno = models.CharField("Turno")
+    hora = models.DateTimeField("Hora",default=timezone.now)
+    PA = models.CharField("PA", max_lengyh = 10)
+    Dx = models.CharField("Dx", max_lengyh = 10)
+    observacao = models.TextField("Observação")
+
+
+    def solicitacao(self):
+        self.data_solicitacao = timezone.now()
         self.save()
 
     def __str__(self):
