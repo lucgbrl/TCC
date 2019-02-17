2a504d56c81bf66084bca79758c71829cb5faace
lucgbrl <lucassantos@alu.ufc.br>
2019-02-17 11:24:29 -0300

    Corrigindo alguns erros no Acompanhamento da pressão arterial

--- mysite/napi/exchange.py
+++ mysite/napi/exchange.py
@@ -3,14 +3,22 @@ from django.conf import settings
 from django.db import models
 
 class Acompanhamento_de_Pressao_Arterial_Glicemia(models.Model):
+    #cadastro de titulo
+    cliente = models.CharField(max_length=200)
+
     #inteiros
     pai = models.ForeignKey("PAI", 'Prontuario_de_Atendimento_Integral_PAI' ,on_delete=models.CASCADE)
 
-    #cadastro de titulo
-    Cliente = models.CharField(max_length=200)
-
     #data e hora automatica
     data_de_solicitacao = models.DateTimeField(default=timezone.now)
+
+
+    #cadastro de data do dia (campo do formulario)
+    dia = models.DateTimeField(blank=True, null=True)
+
+    #cadastro do turno e da hora (campo do formulario)
+    turno = models.CharField(max_length = 64, blank=True, null=True))
+    hora = models.DateTimeField(blank=True, null=True)
 
     #PA e Dx (Pressão e Glicemia)
 
@@ -20,16 +28,15 @@ class Acompanhamento_de_Pressao_Arterial_Glicemia(models.Model):
     #textos longos
     observacao = models.TextField("Observação", null = True, blank = True)
 
-    
-    #cadastro de data do dia (campo do formulario)
-    dia = models.DateTimeField(blank=True, null=True)
+
 
     #usuario que realiza o cadastro
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     def publish(self):
         self.dia = timezone.now()
+        self.dia = timezone.now()
         self.save()
     #######
 
     def __str__(self):
-        return self.title
+        return self.cliente
