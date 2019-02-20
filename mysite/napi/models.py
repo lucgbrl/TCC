3d23a0cb6a625ac6bf21ee8a0a1f5c84945f1fdc
lucgbrl <lucassantos@alu.ufc.br>
2019-02-20 16:11:37 -0300

    Edit background on template

--- mysite/napi/models.py
+++ mysite/napi/models.py
@@ -2,24 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-<<<<<<< HEAD
-class PAI(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    numero_registro = models.CharField(max_length=10)
-    created_date = models.DateTimeField(default=timezone.now)
-
-
-class Acomp_pressao_arterial_glicemia(models.Model):
-    responsavel = models.ForeignKey("responsavel pela solicitação", settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    pai = models.ForeignKey("Pai", 'PAI', on_delete=models.CASCADE)
-    data_solicitacao = models.DateTimeField(blank=True, null=True)
-    dia = models.DateTimeField("Dia",default=timezone.now)
-    turno = models.CharField("Turno")
-    hora = models.DateTimeField("Hora",default=timezone.now)
-    PA = models.CharField("PA", max_lengyh = 10)
-    Dx = models.CharField("Dx", max_lengyh = 10)
-    observacao = models.TextField("Observação")
-=======
 # Classes do projeto
 class Prontuario_de_Atendimento_Integral_PAI(models.Model):
 
@@ -46,7 +28,6 @@ class Prontuario_de_Atendimento_Integral_PAI(models.Model):
     Cel = intergerFild("Celular"max_length=11)
     Fixo = intergerFild("Fixo"max_length=10)
     E-mail = models.text("E-mail")
->>>>>>> equipe1
 
     def publish(self):
         self.Data_de_Nascimento = timezone.now()
