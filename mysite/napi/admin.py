aa41ccf2c31eef06f724c775f923a64499206e80
lucgbrl <lucassantos@alu.ufc.br>
2019-03-11 09:35:14 -0300

    Adicionando formulários de avaliação ao projeto

--- mysite/napi/admin.py
+++ mysite/napi/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Prontuario_de_Atendimento_Integral_PAI, Seguimento_terapeutico
 from .exchange import Acompanhamento_de_Pressao_Arterial_Glicemia, Requisicao_Exame_Citopatologico
+from .ava import ava_clinica_med_sample
 
 # Registrando os models provenientes do arquivo .models aqui
 admin.site.register(Prontuario_de_Atendimento_Integral_PAI)
@@ -10,3 +11,7 @@ admin.site.register(Seguimento_terapeutico)
 
 # registrando arquivos provenientes do arquivos .exchange aqui
 #admin.site.register(Acompanhamento_de_Pressao_Arterial_Glicemia)
+
+#registrando modelos provenientes do arquivo ava.py aqui
+
+admin.site.register(ava_clinica_med_sample)
--- mysite/napi/ava.py
+++ mysite/napi/ava.py
@@ -4,8 +4,11 @@ from django.utils import timezone
 from django.conf import settings
 from django.db import models
 
+
 class ava_clinica_med_sample(models.Model):
     #avaliação clínica, medidas e sampla
+
+    #começa com medidas
     data = models.DateTimeField(blank=True, null = True)
     #medidas
     peso = models.FloatField()
@@ -20,5 +23,18 @@ class ava_clinica_med_sample(models.Model):
         ('O+', 'O+'),
         ('O-', 'O-')
     )
-    tipo_sanguineo = models.CharField(max_length = 3,choices = TIPO_SANGUINEO)
-    
+    tipo_sanguineo = models.CharField(max_length = 3, choices = TIPO_SANGUINEO)
+    pa = models.CharField("PA", max_length=32)
+    dx = models.CharField("DX", max_length=32)
+    fr = models.CharField("FR", max_length=32)
+    pulso = models.CharField("Pulso" , max_length = 32)
+    #sampla
+    sinais_sintomas = models.TextField("Sinais e sintomas")
+    alergias = models.TextField("Alergias")
+    medicacoes= models.TextField("Medicações")
+    patologias = models.TextField("Patologias")
+    liquidos = models.TextField("Líquidos")
+    alimentacao = models.TextField("Alimentação")
+    elementos_relacionados = models.TextField("Elementos relacionados  (Alcool e outras drogas) ")
+    data = models.DateTimeField(blank=True, null = True)
+    profissional = models.TextField("Profissional")
