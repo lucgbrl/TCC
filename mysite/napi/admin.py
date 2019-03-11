945a22e846d8a27b6af91c75e60dc2e5ec003493
lucgbrl <lucassantos@alu.ufc.br>
2019-03-11 10:52:44 -0300

    Testando condicionais de campo

--- mysite/napi/admin.py
+++ mysite/napi/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Prontuario_de_Atendimento_Integral_PAI, Seguimento_terapeutico
 from .exchange import Acompanhamento_de_Pressao_Arterial_Glicemia, Requisicao_Exame_Citopatologico
-from .ava import ava_clinica_med_sample
+from .ava import ava_clinica_med_sample, Acomp_farmacoterapico
 
 # Registrando os models provenientes do arquivo .models aqui
 admin.site.register(Prontuario_de_Atendimento_Integral_PAI)
@@ -15,3 +15,4 @@ admin.site.register(Seguimento_terapeutico)
 #registrando modelos provenientes do arquivo ava.py aqui
 
 admin.site.register(ava_clinica_med_sample)
+admin.site.register(Acomp_farmacoterapico)
--- mysite/napi/ava.py
+++ mysite/napi/ava.py
@@ -1,9 +1,10 @@
 #ava.py reune informações de classes de avaliação de diferentes ramos
-
 from django.utils import timezone
 from django.conf import settings
 from django.db import models
+from .models import Prontuario_de_Atendimento_Integral_PAI
 
+#este documento importa o PAI como classe fundamental pois ele é a base para a maioria dos formulários
 
 class ava_clinica_med_sample(models.Model):
     #avaliação clínica, medidas e sampla
@@ -38,3 +39,29 @@ class ava_clinica_med_sample(models.Model):
     elementos_relacionados = models.TextField("Elementos relacionados  (Alcool e outras drogas) ")
     data = models.DateTimeField(blank=True, null = True)
     profissional = models.TextField("Profissional")
+
+
+class Acomp_farmacoterapico(models.Model):
+    #basico
+    num_pai = models.ForeignKey(Prontuario_de_Atendimento_Integral_PAI, on_delete = models.CASCADE)
+    data_ini_acomp = models.DateTimeField(default=timezone.now(), blank = False, null = False)
+    ALERGIAS = (
+        ('Nega', "Nega"),
+        ('Sim', "Sim"),
+        ('Não sabe informar', "Não sabe informar"),
+    )
+    alergias = models.BooleanField()
+    alergias_affirm = models.CharField("Alergias", max_length=255, choices = ALERGIAS, blank = True, null = True)
+
+    def clean(self):
+        if alergias:
+            msg = forms.ValidationError("Este campo deve ser preenchido")
+            self.add_error("alergias_affirm", msg)
+        else:
+            #mantem o banco de dados consistente a partir do erro
+            #campo não selecionado
+            self.cleaned_data['alergias_affirm'] = ''
+
+    def publish(self):
+        self.data_ini_acomp = timezone.now()
+        self.save()
